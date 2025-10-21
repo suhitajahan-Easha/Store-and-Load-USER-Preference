bf4e8eaef8f97f83eace4d484365ebbbb7f2c670
easha <suhitajahaneasha@gmail.com>
2025-10-22 01:00:17 +0600

    2nd

---
+++ index.js
@@ -0,0 +1,5 @@
+const fontSize= document.getElementById("fontSize");
+const bgColor= document.getElementById("bgColor");
+const btn= document.getElementById("btn");
+const main= document.querySelector("main");
+
